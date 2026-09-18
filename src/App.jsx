295199d7a5f7a538dd84b5e31d198feba3b3a4a9
Haritha <harithabeegam@gmail.com>
2026-09-18 21:26:58 +0530

    Added Task 12 pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Task12 from "./Task12";
+import Task12 from "./Task12/Task12";
 
 export default function App() {
   return (
